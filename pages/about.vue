31aab2582a34b5643fbe38f09eead337ce45541b
Fahmi <talk.windows.inblog@live.com>
2020-05-29 10:45:37 +0800

    init

---
+++ pages/about.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-container>
+    <b-row class="m-auto text-center">
+      <h1>This is about</h1>
+      <br />
+      <p>
+        <span
+          >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas vel
+          odit modi, atque eos officiis veniam libero expedita alias repellendus
+          culpa sit quam. Tempore nemo ratione, eveniet voluptatum ab facere
+          veritatis dolorem dolorum illum excepturi perferendis ut asperiores
+          provident. Architecto repudiandae officia consectetur saepe optio
+          animi, doloremque accusamus perspiciatis quos quaerat veritatis
+          facilis quam praesentium, temporibus eius quisquam. Adipisci cumque
+          sit molestiae assumenda repellat quibusdam velit dicta dolor quia
+          quisquam.</span
+        >
+      </p>
+      <br />
+      <p>
+        <span
+          >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          sunt quidem, unde ea dicta eligendi iure rem, quisquam beatae nam
+          excepturi assumenda? Mollitia beatae, omnis tempora reprehenderit
+          eaque explicabo exercitationem cumque deleniti dolores voluptatum
+          temporibus?</span
+        >
+      </p>
+      <hr />
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "About page"
+    };
+  },
+  head() {
+    return {
+      title: this.title
+    };
+  }
+};
+</script>
+
+<style>
+</style>
